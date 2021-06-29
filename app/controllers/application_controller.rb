1a4e34bb5e62a643f6fa326e95b252f0568a0e5d
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-06-29 18:57:07 -0400

    commit tree 77e09dcf2c9862025067f934e96b994f3314e2e1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,45 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  
+  # create
+  get "/recipes/new" do 
+    erb :new
+  end 
+  # show 
+  get "/recipes" do 
+    @recipes = Recipe.all
+    erb :index
+  end
+  # read 
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+  #edit 
+  get "/recipes/:id/edit" do 
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
 
+  # update
+  patch "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    @recipe.assign_attributes(params[:recipe])
+    @recipe.save
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  # create a recipe
+  post "/recipes" do
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  # delete
+  delete "/recipes/:id" do 
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect to "/recipes"
+  end
 end
